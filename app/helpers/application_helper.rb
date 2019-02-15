7e78809c1f30cb475bb3094ce93c12ec7f0c4e06
Amedes Junior <amedes@gmail.com>
2019-02-15 18:16:53 -0300

    After class 56, Feb 15, 2019.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def nome_aplicacao
+    "Crypto Wallet"
+  end
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
