b01b29f87ec36d147ff69a14375382448744c38e
Amedes Junior <amedes@gmail.com>
2019-02-22 16:22:32 -0300

    Usando uma biblioteca JS (Yarn)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby On Rails - [COOKIE]"
+    session[:curso] = "Curso de Ruby On Rails - [SESSION]"
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( coins.css
+                                                  welcome.css
+                                                  mining_types.css
+                                                  scaffolds.css
+                                                  coins.js
+                                                  welcome.js
+                                                  mining_types.js
+                                                )
